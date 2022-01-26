c6a246824c58491414890ca74ab31aafab7f0af3
MalshaL <malsha.13@cse.mrt.ac.lk>
2022-01-27 02:52:01 +1100

    Added generating pages for tags

---
+++ bitsploit/src/templates/homepage.js
@@ -0,0 +1,48 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout";
+import SEO from "../components/seo"
+import Sidebar from "../components/sidebar";
+import PostList from "../components/postlist";
+
+
+const PostListPage = ({data, pageContext}) => {
+    const { edges: posts } = data.allMarkdownRemark
+    return (
+        <Layout>
+            <div className="gridContainer">
+                <PostList
+                    posts={posts}
+                    pageContext={pageContext}/>
+                <Sidebar/>
+            </div>
+            <SEO title="Posts" />
+        </Layout>
+    )
+}
+
+export const postListQuery = graphql`
+  query ($skip: Int, $limit: Int){
+    allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date] }
+      limit: $limit
+      skip: $skip
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "MMMM DD, YYYY")
+            path
+            tags
+            excerpt
+            image
+          }
+        }
+      }
+    }
+  }
+`
+
+export default PostListPage
